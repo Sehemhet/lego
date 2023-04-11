9fee4d66dfe8d71aecbf331547a19f92c4caa554
volkandrew <andreward.volk@gmail.com>
2023-04-12 02:42:05 +0300

    lego project

---
+++ card_hover/card_hover.js
@@ -0,0 +1,40 @@
+let cardOpen = document.querySelector('.wrap_card')
+let titleCard = document.querySelector('.card')
+let cardHidden = document.querySelector('.hidden_card')
+let cardOpenItem = document.querySelector('.card_active')
+let cardHiddenItem = document.querySelector('.hidden_card_active')
+let listCard = document.querySelector('.container_card')
+
+//// работает только первый элемент
+//cardOpen.addEventListener('click', function(){
+//    titleCard.classList.toggle('card_active')
+//    cardHidden.classList.toggle('hidden_card_active')
+//});
+
+////применяется только один стиль
+//const links = document.querySelectorAll('.card');
+//for (let link of links) {
+//  link.addEventListener('click', () => {
+//    let activeLink = document.querySelector('.card.card_active');
+//    if (activeLink) {
+//      activeLink.classList.remove('card_active');
+//    }
+//    link.classList.toggle('card_active');
+//  })
+//}
+
+//Указанная карточка открывается и закрывается, только ПЕРВАЯ закрывается при нажатии на другую
+const buttons = document.querySelectorAll('.wrap_card')
+for (let button of buttons) {
+  button.addEventListener('click', () => {
+    const activeElement = button.querySelector('.wrap_card')
+    if (!activeElement) {
+      document.querySelector('.wrap_card>.card').classList.remove('card_active')
+      document.querySelector('.wrap_card>.hidden_card').classList.remove('hidden_card_active')
+      button.querySelector('.card').classList.toggle('card_active')
+      button.querySelector('.hidden_card').classList.toggle('hidden_card_active')
+    }
+  })
+}
+
+
